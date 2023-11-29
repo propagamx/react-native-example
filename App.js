9688f8cb1e83a3dfa981e2d65b06c89fc0381fbb
Leonardo Velarde <lg.velarde.andrade@gmail.com>
2023-11-29 12:24:40 -0600

    feat: full project tested

--- App.js
+++ App.js
@@ -1,11 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, View, Button } from 'react-native';
+import { usePropaga } from '@propaga/react-native-sdk';
+import { PROPAGA_TOKEN, USER_ID } from '@env'
 
 export default function App() {
+  const [isPropagaActive, setIsPropagaActive] = useState(false);
+  const { 
+    startPropaga,
+    PropagaPaymentComponent,
+    loaded,
+    PropagaButton,
+   } = usePropaga();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {loaded && (
+        <PropagaButton
+          totalAmount={2000}
+          isActive={isPropagaActive}
+          cornerStoreId={USER_ID}
+          token={PROPAGA_TOKEN}
+          onPress={() => {
+            setIsPropagaActive(!isPropagaActive);
+        }} />
+      )}
+
+      <Button title="Pagar" onPress={startPropaga} />
+
+      <PropagaPaymentComponent
+        transaction={{
+          cornerStoreId: USER_ID,
+          products: [{ 
+            externalSKU: '123', 
+            name: 'Pepsi', 
+            quantity: 1 
+          }],
+          totalAmount: 2001,
+          wholesalerTransactionId: 'TRAN-3245',
+        }}
+        user={{
+          phoneNumber: '3121312311',
+          firstName: 'John',
+          lastNames: 'Doe',
+          latitude: '19.44069',
+          longitude: '-99.21',
+        }}
+        config={{
+          token: PROPAGA_TOKEN,
+        }}
+        onSuccessTransaction={(response) => console.log(response)}
+        onErrorTransaction={(error) => console.log(error)}
+      />
     </View>
   );
 }
